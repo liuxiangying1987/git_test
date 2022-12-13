7f3962ccf82e34472c00ad9d389ba2edcfcf915e
merlin <merlin@gmail.com>
2022-12-14 00:58:12 +0700

    dev 2

--- app/src/main/java/com/key/calc/git_test/MainActivity.java
+++ app/src/main/java/com/key/calc/git_test/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("git_fix");
         System.out.println("master1");
         System.out.println("dev");
+        System.out.println("dev1");
     }
 }
